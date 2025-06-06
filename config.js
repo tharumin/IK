8e0f97e7063fed6c3ab8965ed680575b07715637
JawadTechX <120776154+JawadTechXD@users.noreply.github.com>
2025-06-06 22:25:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,6 +21,8 @@ ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
 // change it to 'same' if you want to resend deleted message in same chat     
 WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
+GOODBYE: process.env.GOODBYE || "false",
+// true if want welcome and goodbye msg in groups       
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
 ANTI_LINK: process.env.ANTI_LINK || "true",
